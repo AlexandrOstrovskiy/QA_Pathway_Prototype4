d6fb13860a4b30376341139fdbeab92f3dcead33
AlexandrOstrovskiy <a.ostrovskiy@rj-games.com>
2021-03-04 16:08:24 +0300

    4.1 Done
    Done:
    camera horizontal rotation
    player movement forward and back
    player movement relative to current camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    private GameObject focalPoint;
+
+    [SerializeField]
+    private float playerSpeed = 5.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayerForward();
+    }
+
+    void MovePlayerForward()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRigidbody.AddForce(focalPoint.transform.forward * playerSpeed * forwardInput);
+    }
+}

7a90df6d24c534aa883c72302669824886f9dbde
AlexandrOstrovskiy <a.ostrovskiy@rj-games.com>
2021-03-04 18:09:50 +0300

    4.3 Done
    DamagePowerUp:
    1) Collect Powerup
    2) coroutine timer for powerup
    3) powerup indicator above player
    4) powerup effect - multiplied force to knockback enemies

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,16 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRigidbody;
     private GameObject focalPoint;
 
+    public GameObject powerUpIndicator;
+
     [SerializeField]
     private float playerSpeed = 5.5f;
+
+    [SerializeField]
+    private bool hasPowerUp;
+
+    [SerializeField]
+    private float powerUpDamageStrength = 15.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +28,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, 2.0f, 0);
         MovePlayerForward();
     }
 
@@ -28,5 +37,39 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRigidbody.AddForce(focalPoint.transform.forward * playerSpeed * Time.deltaTime * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            powerUpIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountDownRoutine());
+        }
+    }
+
+    IEnumerator PowerUpCountDownRoutine()
+    {
+        yield return new WaitForSeconds(8);
+        hasPowerUp = false;
+        powerUpIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("FATALITY");
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpDamageStrength * 50 * Time.deltaTime, ForceMode.Impulse);;
+
+        }
+
     }
 }
